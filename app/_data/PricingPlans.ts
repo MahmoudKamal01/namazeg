41f14bc114be5604c96bd5d8c08b6b136a813ab4
MahmoudKamal01 <MahmoudKamalmk01@gmail.com>
2024-06-21 10:51:36 +0300

    fixed plan link

--- app/_data/PricingPlans.ts
+++ app/_data/PricingPlans.ts
@@ -1,6 +1,6 @@
 export const pricingPlans = [
   {
-    link: "https://buy.stripe.com/test_28o5kreGgbyodyMbII",
+    link: "https://buy.stripe.com/test_eVa9AH1Tu31SbqE001",
     price: 7.99,
     priceId: "prod_QIRPMMhzRuA2aJ",
     duration: "Monthly",
@@ -12,7 +12,7 @@ export const pricingPlans = [
     communitySupport: false,
   },
   {
-    link: "https://buy.stripe.com/test_eVa9AH1Tu31SbqE001",
+    link: "https://buy.stripe.com/test_28o5kreGgbyodyMbII",
     price: 49.0,
     priceId: "prod_QIRKirGtr6omst",
     duration: "Yearly",
